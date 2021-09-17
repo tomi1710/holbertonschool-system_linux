d542b7bea451b10a020c1c0c01ddbee3fe973834
tomi1710 <2294@holbertonschool.com>
2021-09-17 00:12:36 +0000

    add

--- 0x05-libasm/0-strlen.asm
+++ 0x05-libasm/0-strlen.asm
@@ -1,10 +1,32 @@
-strlen:
-    mov ebx,0
-    strlen_loop:
-        cmp byte [eax+ebx],0
-        je strlen_end
-        inc ebx
-        jmp strlen_loop
-    strlen_end:
-        inc ebx
-        ret
+	bits 64
+
+	section .text
+	global asm_strlen
+
+asm_strlen:
+	push rbp
+	mov rbp, rsp
+
+	push rbx
+	push rcx
+
+	mov rbx, rdi
+	mov rcx, 0
+	jmp entry_i
+
+iter_i:
+	inc rcx
+entry_i:
+	cmp byte [rbx + rcx ], 0x00
+	jne iter_i
+
+end:
+	mov rax, rcx
+
+	pop rcx
+	pop rbx
+
+	mov rsp, rbp
+	pop rbp
+
+	ret
