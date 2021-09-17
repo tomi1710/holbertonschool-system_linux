fc31e225fcec349321d3a6b6c05283aaab97490e
tomi1710 <2294@holbertonschool.com>
2021-09-17 00:21:59 +0000

    fix

--- 0x05-libasm/libasm.h
+++ 0x05-libasm/libasm.h
@@ -3,5 +3,7 @@
 
 int asm_strlen(char *a);
 int asm_strcmp(const char *s1, const char *s2);
+int asm_strncmp(const char *s1, const char *s2, size_t n);
+char *asm_strchr(const char *s, int c);
 
 #endif
